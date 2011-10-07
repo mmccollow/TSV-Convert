81a9e477e899afa56792ec0ce67e44e9764a62d5
Matt <mmccollow@gmail.com>
2011-10-07 12:42:11 -0400

    Fleshed out functions; Extended DC class

--- convert.py
+++ convert.py
@@ -1,11 +1,19 @@
 #!bin/python
 
-import dublincore as dc
+from dublincore import dublinCoreMetadata
 import csv
 from sys import argv
 from xml.dom.minidom import Document
 
+DC_NS = 'http://purl.org/dc/elements/1.1/'
+
+class DublinCore(dublinCoreMetadata):
+	""" Add toprettyxml() func so we can treat this like an xml.dom.minidom.Document """
+	def toprettyxml():
+		self.makeXML(DC_NS)
+
 class TabFile(object):
+	""" A dialect for the csv.DictReader constructor """
 	delimiter = '\t'
 
 def usage():
@@ -16,21 +24,42 @@ def parse(fn):
 	""" Parse a TSV file """
 	try:
 		fp = open(fn)
-		tsv = csv.reader(fp, TabFile)
+		fields = fp.readline().rstrip('\n').split('\t')
+		tsv = csv.DictReader(fp, fieldnames=fields dialect=TabFile)
+		while row = tsv.next():
+			makedc(row)
+			makexml(row)
 	except IOError as (errno, strerror):
 		print "Error ({0}): {1}".format(errno, strerror)
 		raise SystemExit
-	return tsv
+	fp.close()
 
-def makedc(tsv):
+def makedc(row):
 	""" Generate a Dublin Core XML file from a TSV """
-	metadata = dc.dublinCoreMetadata()
+	metadata = DublinCore()
+	metadata.Contributor = row.get('dc:contributor', '')
+	metadata.Coverage = row.get('dc:coverage', '')
+	metadata.Creator = row.get('dc:creator', '')
+	metadata.Date = row.get('dc:date', '')
+	metadata.Description = row.get('dc:description', '')
+	metadata.Format = row.get('dc:format', '')
+	metadata.Identifier = row.get('dc:identifier', '')
+	metadata.Language = row.get('dc:language', '')
+	metadata.Publisher = row.get('dc:publisher', '')
+	metadata.Relation = row.get('dc:relation', '')
+	metadata.Rights = row.get('dc:rights', '')
+	metadata.Source = row.get('dc:source', '')
+	metadata.Subject = row.get('dc:subject', '')
+	metadata.Title = row.get('dc:title', '')
 	return metadata
 
-def makexml(tsv):
+def makexml(row):
 	""" Generate a custom XML file from a TSV """
 	doc = Document()
 	return doc
+
+def writefile(name, obj):
+	pass
 
 def chkarg(arg):
 	""" Was a TSV file specified? """
@@ -38,9 +67,7 @@ def chkarg(arg):
 
 if __name__ == "__main__":
 	if chkarg(argv[0]):
-		tsv = parse(argv[0])
-		makedc(tsv)
-		makexml(tsv)
+		parse(argv[0])
 	else:
 		usage()
 
