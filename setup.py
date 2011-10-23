d95b98ad6132cdb31fd7aa5ea5c2bd5ed5abe0ff
nruest <ruestn@gmail.com>
2011-10-22 20:59:03 -0700

    add requirements to easy_install bootstrap

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
-from setuptools import setup
+#bootstrap easy_install
+import ez_setup
+ez_setup.use_setuptools()
+
+  from setuptools import setup, find_packages
 
 description = \
 """
 This package is used to create individual Dublin Core and Macrepo xml records from a given tsv file.
 """
+
+install_requires = []
+try:
+    import DublinCore
+    except ImportError:
+        install_requires.append('dublincore>=1.0')
 
 setup (
     name = 'tsv-convert',
@@ -11,7 +21,7 @@ setup (
     url = 'https://github.com/mmccollow/TSV-Convert',
     author = 'Matt McCollow, Nick Ruest',
     author_email = 'mccollo@mcmaster.ca',
-    py_modules = ['dublincore'],
+    py_modules = ['DublinCore', 'ez_setup'],
     scripts = ['tsv-convert.py'],
     description = description,
     platforms = ['POSIX'],
